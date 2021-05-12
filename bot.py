c9397dcd7cd301ccf595acaa5a3e314953aacc63
LeviFX <82326386+LeviFX@users.noreply.github.com>
2021-05-12 14:11:16 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import tweepy
+import time
+
+consumer_key = '# api key'
+consumer_secret = '#api key secret' 
+
+key = '# acces token'
+secret = '# acces token secret'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+
+api = tweepy.API(auth)
+
+def reply():
+    for tweet in tweepy.Cursor(api.search, q='#hashtag').items(1):
+        print(str(tweet.id) + ' - ' + tweet.text)
+        api.update_status("@" + tweet.user.screen_name + " Response text", tweet.id)
+
+
+result = None
+while result is None:
+    try:
+        reply()
+        time.sleep(30)
+        result = None
+    except:
+        pass
+
+    if ValueError:
+        time.sleep(30)
+    else:
+        break
